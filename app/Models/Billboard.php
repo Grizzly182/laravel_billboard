ed603bcbb04c4964e64f30e290f6f857b9bffc38
Mikhail Belousov <tiger.mananticot@yandex.ru>
2025-10-27 19:57:00 +0300

    created billboards link to user

--- app/Models/Billboard.php
+++ app/Models/Billboard.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Billboard extends Model
 {
     protected $fillable = ['title', 'content', 'price'];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -6,4 +6,6 @@
         <h2>{{ $billboard->title }}</h2>
         <p>{{ $billboard->content }}</p>
         <p>{{ $billboard->price }} руб.</p>
+        <p><b>Автор:</b> {{ $billboard->user->name }}</p>
+        <a href="{{route('home')}}" class="btn btn-primary">Назад</a>
 @endsection
