977c32b903857dc438f1cb951d09454e50625af1
Mikhail Belousov <tiger.mananticot@yandex.ru>
2025-10-28 14:11:02 +0300

    added auth facade

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BillboardsController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,3 +16,6 @@ use App\Http\Controllers\BillboardsController;
 
 Route::get('/', [BillboardsController::class, 'index'])->name('home');
 Route::get('/{billboard}', [BillboardsController::class, 'detail'])->name('billboard.detail');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
