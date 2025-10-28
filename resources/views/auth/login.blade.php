093ac456cd75a685bd8cea62742ac7aa960d6c3a
Mikhail Belousov <tiger.mananticot@yandex.ru>
2025-10-28 14:47:10 +0300

    small views tweaks

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('title', 'Вход')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('title', 'Восстановление пароля')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
